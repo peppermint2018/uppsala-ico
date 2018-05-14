f33d5517b43fdf961182e554ddf9f5af7d2d653e
pat-kim <khw1673@gmail.com>
2018-05-15 06:47:08 +0900

    Update UppsalaToken.sol

--- contracts/UppsalaToken.sol
+++ contracts/UppsalaToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.23;
 import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
 contract UppsalaToken is MintableToken {
-	string public constant name = 'UPPSALA Token';
+	string public constant name = 'SENTINEL PROTOCOL Token';
 	string public constant  symbol = 'UPP';
 	uint8 public constant decimals = 18;
 }

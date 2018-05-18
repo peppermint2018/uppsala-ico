42d8fc7c11bb618a963bce96e4c1268e853c95eb
Dayeol Lee <dayeol@berkeley.edu>
2018-05-19 00:59:20 +0900

    Pausable Token

--- contracts/UppsalaToken.sol
+++ contracts/UppsalaToken.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.4.23;
 
 import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/PausableToken.sol';
 
-contract UppsalaToken is MintableToken {
+contract UppsalaToken is MintableToken, PausableToken {
 	string public constant name = 'Sentinel Protocol';
 	string public constant  symbol = 'UPP';
 	uint8 public constant decimals = 18;
